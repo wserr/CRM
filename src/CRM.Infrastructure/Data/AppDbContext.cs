61793da4cb0c586a202d12bfb469a6c9b4d5b86b
Willem Serruys <willem.serruys@proceedix.com>
2020-12-03 20:51:34 +0100

    Add Create Endpoint

--- src/CRM.Infrastructure/Data/AppDbContext.cs
+++ src/CRM.Infrastructure/Data/AppDbContext.cs
@@ -27,6 +27,8 @@ namespace CRM.Infrastructure.Data
 
         public DbSet<ToDoItem> ToDoItems { get; set; }
 
+        public DbSet<Student> Students { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
---
+++ src/CRM.Web/Endpoints/Students/Create.NewStudentRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Web.Endpoints.Students
+{
+    public class NewStudentRequest
+    {
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string Email { get; set; }
+    }
+}
