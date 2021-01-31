36630857de5a7f7ce1ea3f8ec1c99c3f743fd70c
Willem Serruys <willem.serruys@proceedix.com>
2021-01-31 11:26:54 +0100

    added create and delete payment

---
+++ src/CRM.Web/Endpoints/Payments/Create.NewPaymentRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Web.Endpoints.Payments
+{
+    public class NewPaymentRequest
+    {
+        public decimal Amount { get; set; }
+        public int StudentId { get; set; }
+    }
+}
