b41ddce1bfcc37cc3acaa45974c880f882c279f2
Willem Serruys <willem.serruys@proceedix.com>
2021-01-31 11:11:39 +0100

    payment list endpoint

---
+++ src/CRM.Web/Endpoints/Payments/List.ListPaymentResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Web.Endpoints.Payments
+{
+    public class ListPaymentResponse
+    {
+        public DateTime TimeStamp { get; set; }
+
+        public Decimal Amount { get; set; }
+
+    }
+}
