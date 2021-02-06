9b182ed2e02dfc0bfc3a09d4d8544974ec5a0ace
Willem Serruys <willem.serruys@proceedix.com>
2021-02-06 06:46:18 +0100

    restructure project

---
+++ Api/src/CRM.Infrastructure/StartupSetup.cs
@@ -0,0 +1,14 @@
+﻿using CRM.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CRM.Infrastructure
+{
+    public static class StartupSetup
+    {
+        public static void AddDbContext(this IServiceCollection services, string connectionString) =>
+            services.AddDbContext<AppDbContext>(options =>
+                options
+            .UseNpgsql(connectionString).UseLowerCaseNamingConvention()); // will be created in web project root
+    }
+}
